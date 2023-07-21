48ef1811af92c014e9a95c5180c6c1f6afbc3295
bulentoral <96498139+bulent017@users.noreply.github.com>
2023-07-21 09:33:52 +0300

    Creating Repository is done

--- app/src/main/java/com/example/cryptocurrencyapp/data/remote/CoinPaprikaApi.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/data/remote/CoinPaprikaApi.kt
@@ -1,5 +1,6 @@
 package com.example.cryptocurrencyapp.data.remote
 
+import com.example.cryptocurrencyapp.data.remote.dto.CoinDetailDto
 import com.example.cryptocurrencyapp.data.remote.dto.CoinDto
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -9,7 +10,7 @@ interface CoinPaprikaApi {
     suspend fun getCoins() : List<CoinDto> // suspend fonksiyonlar coroutinler ile çalışmaya izin verir
 
     @GET("/v1/coins{coinId}")
-    suspend fun getCoinById(@Path("coinId") coinId : String)
+    suspend fun getCoinById(@Path("coinId") coinId : String) :CoinDetailDto //@Path anotasyonu, Retrofit kütüphanesinin bir parçasıdır ve HTTP isteklerinin URL'indeki değişken kısımları tanımlamak için kullanılır.
 
 
 
